8b5f1ec73683bae47692c30d0410deb6014fac9a
Marco Grassi <marco.g92@hotmail.it>
2022-09-17 00:45:23 +0200

    fix: changed names in controller and service

--- src/main/java/com/example/demo/Service/CitazioneService.java
+++ src/main/java/com/example/demo/Service/CitazioneService.java
@@ -3,6 +3,7 @@ package com.example.demo.Service;
 import com.example.demo.model.Citazione;
 import com.example.demo.model.Utente;
 import com.example.demo.repositories.CitazioneRepository;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,17 +19,21 @@ public class CitazioneService {
         this.citazioneRepository = citazioneRepository;
     }
    
-     public Citazione postCitazione (Citazione citazione){
+     public Citazione post (Citazione citazione){
          Random rd = new Random();
          citazione.setIdCitazione(rd.nextInt());
          return citazioneRepository.save(citazione);
      }      
    
-   public Optional<Citazione> getCitazione(Integer id){
+   public Optional<Citazione> getById(Integer id){
        return citazioneRepository.findById(id);
    }
    
-    public void deleteCitazione(Integer id){
+    public List<Citazione> getAll(){
+       return citazioneRepository.findAll();
+   }
+      
+    public void deleteById(Integer id){
         citazioneRepository.deleteById(id);
    }
 }
